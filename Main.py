2b608eee5ad4d2319a3045b082b04544cbf93999
PArangSae_KoreanCoder_한국코더 <72248972+PArangSae@users.noreply.github.com>
2020-10-02 19:23:56 +0900

    Main

---
+++ Main.py
@@ -0,0 +1,129 @@
+print("PArangBot version 1.0")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("PArangBot ready to start.")
+
+print("")
+print("")
+print("")
+print("")
+lang = input("Write your language.(ko/en) >>   ")
+print("")
+print("")
+print("")
+print("'" + lang + "'" + " language ready")
+while lang == "en":
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    ans = input("[ Hi, I am PArangBot. ]   ")
+    if ans == "Hi":
+        print("")
+        print("[ Hello. THis is PArangBot. This is made by a person named PArangSae. ]")
+    else:
+        if ans == "Hello":
+            print("")
+            print("[ Hello. THis is PArangBot. This is made by a person named PArangSae. ]")
+        else:
+            if ans == "hi":
+                print("")
+                print("[ Hello. THis is PArangSae AI. This is made by a person named PArangSae. ]")
+            else:
+                if ans == "hello":
+                    print("")
+                    print("[ Hello. THis is PArangBot. This is made by a person named PArangSae. ]")
+                else:
+                    if ans == "I love you":
+                        print("")
+                        print("[ Thank you for loving PArangBot. We will find you with a better service later. ]")
+                    else:
+                        if ans == "break":
+                            print("")
+                            break
+                        else:
+                            print("")
+                            print("[ Sorry. PArangBot doesn't have an answer setten for the word, or you missed the word. Sorry for bad services. We will find you with a better PArangBot sooner. ]")
+
+while lang == "ko":
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    ans = input("[ 안녕하세요, PArangBot입니다. ]   ")
+    if ans == "안녕":
+        print("")
+        print("[ 안녕하세요. 이 봇의 이름은 PArangBot입니다. PArangSae라는 사람이 개발했습니다. ]")
+    else:
+        if ans == "안녕하세요":
+            print("")
+            print("[ 안녕하세요! PArangBot입니다! ]")
+        else:
+            if ans == "하이":
+                print("")
+                print("[ 안녕하세요! 파랑새는 춤춥니다! ]")
+            else:
+                if ans == "사랑해":
+                    print("")
+                    print("[ 저도이죠;) ]")
+                else:
+                    if ans == "고마워":
+                        print("")
+                        print("[ 파랑봇을 사랑해주셔서 감사합니다! 앞으로도 더 좋은 서비스로 보답하겠습니다! ]")
+                    else:
+                        if ans == "종료":
+                            print("")
+                            break
+                        else:
+                             if ans == "울랄라":
+                                 print("")
+                                 print("[ 울랄랄라~ ㅎ ]")
+                             else:
+                                 print("")
+                                 print("[ 죄송합니다 T.T 말씀하신 단어를 알아듣지 못했습니다. 가능한 명령어를 시도하시거나 맞춤법을 확인해주세요. 불편을 끼쳐드려 죄송합니다.. 앞으로 더 좋은 서비스로 돌아오겠습니다... ]")
+
+
